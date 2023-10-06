256a1c263fa439f4fa9cd9b6f9696f9b9176b532
Jordan Wanlass <jwanlass8@gmail.com>
2023-10-06 15:25:28 -0600

    uncomment out line

--- src/App.java
+++ src/App.java
@@ -4,7 +4,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         Spreadsheet spreadsheet = new Spreadsheet("/Users/jordanwanlass/Desktop/Windfall/src/input.csv");
         spreadsheet.evalSpreadsheet();
-        // spreadsheet.printSpreadsheet();
+        spreadsheet.printSpreadsheet();
         spreadsheet.downloadSpreadsheet();
     }
 }
